e7e458cf0f20273b14f84ee69b0933a5b18ea191
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-01-17 21:07:09 +0530

    added documentation

--- config-server/src/main/java/com/github/muhammedshaheer/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/github/muhammedshaheer/configserver/ConfigServerApplication.java
@@ -6,6 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * @author Muhammed Shaheer
+ * @since 17 January 2021
+ */
+
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
